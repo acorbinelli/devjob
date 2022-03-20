25774ca857d6056b77ecef2d6ec934f265f9dbbb
Alessandro Corbinelli <acorbinelli@gmail.com>
2022-03-20 20:42:26 +0200

    create about page create contact page

--- src/Pages/Contact/Contact.tsx
+++ src/Pages/Contact/Contact.tsx
@@ -1,5 +1,8 @@
-import { Box, useTheme, Paper, Fade } from "@mui/material";
-
+import { Box, useTheme, Paper, Fade, IconButton } from "@mui/material";
+import AlternateEmailIcon from "@mui/icons-material/AlternateEmail";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import FiverrLogo from "images/fiverrLogo.png";
 const Contact = () => {
   const theme = useTheme();
   return (
@@ -8,8 +11,43 @@ const Contact = () => {
         mt: 2,
         background: "transparent",
         height: `calc(100vh - ${theme.spacing(25)})`,
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+        [theme.breakpoints.down("sm")]: {
+          flexDirection: "column",
+        },
       }}
-    ></Box>
+    >
+      <IconButton
+        color="secondary"
+        sx={{ height: "fit-content" }}
+        href="mailto:acorbinelli@gmail.com?subject=Your Subject&body=message%20goes%20here"
+      >
+        <AlternateEmailIcon sx={{ fontSize: theme.typography.h1.fontSize }} />
+      </IconButton>
+      <IconButton
+        color="secondary"
+        sx={{ height: "fit-content" }}
+        onClick={() => window.open("https://www.linkedin.com/in/alessandro-corbinelli/", "_blank")}
+      >
+        <LinkedInIcon sx={{ fontSize: theme.typography.h1.fontSize }} />
+      </IconButton>
+      <IconButton
+        color="secondary"
+        sx={{ height: "fit-content" }}
+        onClick={() => window.open("https://github.com/acorbinelli", "_blank")}
+      >
+        <GitHubIcon sx={{ fontSize: theme.typography.h1.fontSize }} />
+      </IconButton>
+      <IconButton
+        color="secondary"
+        sx={{ height: "fit-content" }}
+        onClick={() => window.open("https://www.fiverr.com/acorbinelli", "_blank")}
+      >
+        <img src={FiverrLogo} alt="fiverr" height={theme.spacing(10)} />
+      </IconButton>
+    </Box>
   );
 };
 
