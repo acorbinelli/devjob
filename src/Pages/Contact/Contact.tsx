4534a2fbec1d6ad02104961ded0638e3aa4d03ad
Alessandro Corbinelli <acorbinelli@gmail.com>
2022-03-20 20:47:50 +0200

    add slide anim

--- src/Pages/Contact/Contact.tsx
+++ src/Pages/Contact/Contact.tsx
@@ -1,4 +1,4 @@
-import { Box, useTheme, Paper, Fade, IconButton } from "@mui/material";
+import { Box, useTheme, Paper, Fade, IconButton, Slide } from "@mui/material";
 import AlternateEmailIcon from "@mui/icons-material/AlternateEmail";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import GitHubIcon from "@mui/icons-material/GitHub";
@@ -17,36 +17,45 @@ const Contact = () => {
         [theme.breakpoints.down("sm")]: {
           flexDirection: "column",
         },
+        overflow: "hidden",
       }}
     >
-      <IconButton
-        color="secondary"
-        sx={{ height: "fit-content" }}
-        href="mailto:acorbinelli@gmail.com?subject=Your Subject&body=message%20goes%20here"
-      >
-        <AlternateEmailIcon sx={{ fontSize: theme.typography.h1.fontSize }} />
-      </IconButton>
-      <IconButton
-        color="secondary"
-        sx={{ height: "fit-content" }}
-        onClick={() => window.open("https://www.linkedin.com/in/alessandro-corbinelli/", "_blank")}
-      >
-        <LinkedInIcon sx={{ fontSize: theme.typography.h1.fontSize }} />
-      </IconButton>
-      <IconButton
-        color="secondary"
-        sx={{ height: "fit-content" }}
-        onClick={() => window.open("https://github.com/acorbinelli", "_blank")}
-      >
-        <GitHubIcon sx={{ fontSize: theme.typography.h1.fontSize }} />
-      </IconButton>
-      <IconButton
-        color="secondary"
-        sx={{ height: "fit-content" }}
-        onClick={() => window.open("https://www.fiverr.com/acorbinelli", "_blank")}
-      >
-        <img src={FiverrLogo} alt="fiverr" height={theme.spacing(10)} />
-      </IconButton>
+      <Slide in easing={{ enter: "cubic-bezier(1,-0.21,0,1.24)" }} timeout={700}>
+        <IconButton
+          color="secondary"
+          sx={{ height: "fit-content" }}
+          href="mailto:acorbinelli@gmail.com?subject=Your Subject&body=message%20goes%20here"
+        >
+          <AlternateEmailIcon sx={{ fontSize: theme.typography.h1.fontSize }} />
+        </IconButton>
+      </Slide>
+      <Slide in direction="up" easing={{ enter: "cubic-bezier(1,-0.21,0,1.24)" }} timeout={700}>
+        <IconButton
+          color="secondary"
+          sx={{ height: "fit-content" }}
+          onClick={() => window.open("https://www.linkedin.com/in/alessandro-corbinelli/", "_blank")}
+        >
+          <LinkedInIcon sx={{ fontSize: theme.typography.h1.fontSize }} />
+        </IconButton>
+      </Slide>
+      <Slide in easing={{ enter: "cubic-bezier(1,-0.21,0,1.24)" }} timeout={700}>
+        <IconButton
+          color="secondary"
+          sx={{ height: "fit-content" }}
+          onClick={() => window.open("https://github.com/acorbinelli", "_blank")}
+        >
+          <GitHubIcon sx={{ fontSize: theme.typography.h1.fontSize }} />
+        </IconButton>
+      </Slide>
+      <Slide in direction="up" easing={{ enter: "cubic-bezier(1,-0.21,0,1.24)" }} timeout={700}>
+        <IconButton
+          color="secondary"
+          sx={{ height: "fit-content" }}
+          onClick={() => window.open("https://www.fiverr.com/acorbinelli", "_blank")}
+        >
+          <img src={FiverrLogo} alt="fiverr" height={theme.spacing(10)} />
+        </IconButton>
+      </Slide>
     </Box>
   );
 };
